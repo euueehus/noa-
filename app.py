34dd865f6b31e68964b1ce7e53b74b170410122f
euueehus <euueehusssss@gmail.com>
2025-12-02 15:59:20 +0800

    new

--- app.py
+++ app.py
@@ -54,5 +54,10 @@ def delta():
     return render_template("delta.html")
 
 
+@app.route("/calss")
+def calss():
+    return render_template("calss.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000), host="0.0.0.0")
